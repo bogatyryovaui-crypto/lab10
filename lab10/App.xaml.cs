51b5bca80f093ac4affc13f205357ba02013cd51
bogatyryovaui-crypto <bogatyryovaui@mer.ci.nsu.ru>
2026-05-07 13:43:19 +0700

    Update App.xaml.cs

--- lab10/App.xaml.cs
+++ lab10/App.xaml.cs
@@ -11,7 +11,28 @@ namespace lab10
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class App : Application
+     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IDialogService, DialogService>();
+            services.AddTransient<MainViewModel>();
+
+            services.AddSingleton<MainWindow>(sp =>
+            {
+                var window = new MainWindow();
+                window.DataContext = sp.GetRequiredService<MainViewModel>();
+                return window;
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            var mainWindow = provider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
